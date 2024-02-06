7d4c562faa3e21f6ba845938522fd4d191862bef
SeWZC <36623158+SeWZC@users.noreply.github.com>
2024-02-06 21:13:18 +0800

    拆开 VectorFactory.cs 到 NumFactory.cs

---
+++ SeWzc.Numerics.Tests/NumFactory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SeWzc.Numerics.Tests;
+
+public static class NumFactory
+{
+    #region 静态变量
+
+    public const int Count = 1000;
+
+    #endregion
+
+    #region 静态方法
+
+    public static IEnumerable<T> RandomCreateRange<T>(Func<Random, T> creator, Random random)
+    {
+        return Enumerable.Range(0, Count).Select(_ => creator(random));
+    }
+
+    public static ImmutableArray<T> RandomCreateRange<T>(IEnumerable<T> other, Func<Random, T> creator, Random random)
+    {
+        var array = other.Concat(RandomCreateRange(creator, random)).ToArray();
+        random.Shuffle(array);
+        return array.ToImmutableArray();
+    }
+
+    #endregion
+}
+
+public static class NumFactory<TNum>
+    where TNum : unmanaged, INumber<TNum>
+{
+    #region 静态变量
+
+    public static ImmutableArray<TNum> RandomNum { get; }
+
+    #endregion
+
+    #region 静态方法
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TNum NextNum(Random random)
+    {
+        if (typeof(TNum) == typeof(double))
+            return Unsafe.BitCast<long, TNum>(random.NextInt64());
+        if (typeof(TNum) == typeof(float))
+            return Unsafe.BitCast<int, TNum>(random.Next());
+        throw new NotSupportedException();
+    }
+
+    #endregion
+
+    #region 构造函数
+
+    static NumFactory()
+    {
+        if (typeof(TNum) != typeof(double) && typeof(TNum) != typeof(float))
+            throw new NotSupportedException();
+
+        RandomNum = NumFactory.RandomCreateRange([TNum.Zero, TNum.One], NextNum, new Random(0x225ca3d));
+    }
+
+    #endregion
+}
--- SeWzc.Numerics.Tests/VectorFactory.cs
+++ SeWzc.Numerics.Tests/VectorFactory.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Numerics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace SeWzc.Numerics.Tests;
 
@@ -12,37 +9,34 @@ public static class VectorFactory<TVector, TNum>
     where TVector : unmanaged, IVector<TVector, TNum>
     where TNum : unmanaged, INumber<TNum>
 {
-    private const int Count = 1000;
+    #region 静态变量
 
-    static VectorFactory()
+    public static ImmutableArray<TVector> RandomVectors1 { get; } = NumFactory.RandomCreateRange([TVector.Zero], CreateRandomVector, new Random(0x79aefaa7));
+    public static ImmutableArray<TVector> RandomVectors2 { get; } = NumFactory.RandomCreateRange([TVector.Zero], CreateRandomVector, new Random(0x43d1d9d1));
+
+    #endregion
+
+    #region 静态方法
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Test(Action<TVector> action)
     {
-        Random random1 = new(0x79aefaa7);
-        TVector[] randomVectors1 = [TVector.Zero, ..RangeSelect(_ => CreateRandomVector(random1))];
-        random1.Shuffle(randomVectors1);
-        RandomVectors1 = ImmutableCollectionsMarshal.AsImmutableArray(randomVectors1);
-
-        Random random2 = new(0x43d1d9d1);
-        TVector[] randomVectors2 = [TVector.Zero, ..RangeSelect(_ => CreateRandomVector(random2))];
-        random2.Shuffle(randomVectors2);
-        RandomVectors2 = ImmutableCollectionsMarshal.AsImmutableArray(randomVectors2);
-
-        Random random3 = new(0x1e03afb6);
-        TVector[] randomVectors3 = [TVector.Zero, ..RangeSelect(_ => CreateRandomVector(random3))];
-        random3.Shuffle(randomVectors3);
-        RandomVectors3 = ImmutableCollectionsMarshal.AsImmutableArray(randomVectors3);
-
-        Random randomNum = new(0x225ca3d);
-        RandomScalars = [TNum.Zero, ..Enumerable.Range(0, Count).Select(_ => NextNum(randomNum))];
+        for (var i = 0; i < NumFactory.Count; i++)
+            action(RandomVectors1[i]);
     }
 
-    private static ImmutableArray<TVector> RandomVectors1 { get; }
-    private static ImmutableArray<TVector> RandomVectors2 { get; }
-    private static ImmutableArray<TVector> RandomVectors3 { get; }
-    private static ImmutableArray<TNum> RandomScalars { get; }
-
-    private static IEnumerable<TVector> RangeSelect(Func<int, TVector> selector)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Test(Action<TVector, TNum> action)
     {
-        return Enumerable.Range(0, Count).Select(selector);
+        for (var i = 0; i < NumFactory.Count; i++)
+            action(RandomVectors1[i], NumFactory<TNum>.RandomNum[i]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Test(Action<TVector, TVector> action)
+    {
+        for (var i = 0; i < NumFactory.Count; i++)
+            action(RandomVectors1[i], RandomVectors2[i]);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -51,46 +45,10 @@ public static class VectorFactory<TVector, TNum>
         var dimension = TVector.Dimension;
         var objects = new object[dimension];
         for (var i = 0; i < dimension; i++)
-            objects[i] = NextNum(random);
+            objects[i] = NumFactory<TNum>.NextNum(random);
 
         return (TVector)Activator.CreateInstance(typeof(TVector), objects)!;
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static TNum NextNum(Random random)
-    {
-        if (typeof(TNum) == typeof(double))
-            return Unsafe.BitCast<long, TNum>(random.NextInt64());
-        if (typeof(TNum) == typeof(float))
-            return Unsafe.BitCast<int, TNum>(random.Next());
-        throw new NotSupportedException();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Test(Action<TVector> action)
-    {
-        for (var i = 0; i < Count; i++)
-            action(RandomVectors1[i]);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Test(Action<TVector, TNum> action)
-    {
-        for (var i = 0; i < Count; i++)
-            action(RandomVectors1[i], RandomScalars[i]);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Test(Action<TVector, TVector> action)
-    {
-        for (var i = 0; i < Count; i++)
-            action(RandomVectors1[i], RandomVectors2[i]);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Test(Action<TVector, TVector, TVector> action)
-    {
-        for (var i = 0; i < Count; i++)
-            action(RandomVectors1[i], RandomVectors2[i], RandomVectors3[i]);
-    }
+    #endregion
 }
