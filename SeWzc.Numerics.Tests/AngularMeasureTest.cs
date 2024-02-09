c570a0f504d5d5a6d481c986f5f085ebd359c70c
SeWZC <36623158+SeWZC@users.noreply.github.com>
2024-02-10 00:07:50 +0800

    添加角度静态字段、常用三角函数方法、比较运算符重载

---
+++ SeWzc.Numerics.Tests/AngularMeasureTest.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace SeWzc.Numerics.Tests;
+
+[TestSubject(typeof(AngularMeasure))]
+public class AngularMeasureTest
+{
+    #region 成员方法
+
+    [Theory(DisplayName = "测试从角度创建角。")]
+    [MemberData(nameof(AngleData))]
+    public void FromDegreeTest(AngularMeasure angle)
+    {
+        Assert.Equal(angle, AngularMeasure.FromDegree(angle.Degree), NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Theory(DisplayName = "测试从弧度创建角。")]
+    [MemberData(nameof(AngleData))]
+    public void FromRadianTest(AngularMeasure angle)
+    {
+        Assert.Equal(angle, AngularMeasure.FromRadian(angle.Radian), NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Theory(DisplayName = "测试角的加法。")]
+    [MemberData(nameof(AngleAddTestData))]
+    public void AddTest(AngularMeasure angle1, AngularMeasure angle2, AngularMeasure expected)
+    {
+        Assert.Equal(expected, angle1 + angle2, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Theory(DisplayName = "测试角的减法。")]
+    [MemberData(nameof(AngleAddTestData))]
+    public void SubtractTest(AngularMeasure expected, AngularMeasure angle1, AngularMeasure angle2)
+    {
+        Assert.Equal(expected, angle2 - angle1, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Fact(DisplayName = "测试角的乘法。")]
+    public void MultiplyTest()
+    {
+        Assert.Equal(AngularMeasure.Pi, AngularMeasure.HalfPi * 2, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.Tau, AngularMeasure.Pi * 2, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.Tau, AngularMeasure.HalfPi * 4, NumericsEqualHelper.IsAlmostEqual);
+
+        Assert.Equal(AngularMeasure.Pi, 2 * AngularMeasure.HalfPi, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.Tau, 2 * AngularMeasure.Pi, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.Tau, 4 * AngularMeasure.HalfPi, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Fact(DisplayName = "测试角的数除。")]
+    public void DivideNumTest()
+    {
+        Assert.Equal(AngularMeasure.HalfPi, AngularMeasure.Pi / 2, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.HalfPi, AngularMeasure.Tau / 4, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.Pi, AngularMeasure.Tau / 2, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Fact(DisplayName = "测试角的除法。")]
+    public void DivideTest()
+    {
+        Assert.Equal(2, AngularMeasure.Pi / AngularMeasure.HalfPi, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(4, AngularMeasure.Tau / AngularMeasure.HalfPi, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(2, AngularMeasure.Tau / AngularMeasure.Pi, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Fact(DisplayName = "测试角的比较。")]
+    public void CompareTest()
+    {
+        Assert.True(AngularMeasure.Zero < AngularMeasure.HalfPi);
+        Assert.True(AngularMeasure.HalfPi < AngularMeasure.Pi);
+        Assert.True(AngularMeasure.Pi < AngularMeasure.HalfPi + AngularMeasure.Pi);
+        Assert.True(AngularMeasure.HalfPi + AngularMeasure.Pi < AngularMeasure.Tau);
+
+        Assert.True(AngularMeasure.Zero <= AngularMeasure.HalfPi);
+        Assert.True(AngularMeasure.HalfPi <= AngularMeasure.Pi);
+        Assert.True(AngularMeasure.Pi <= AngularMeasure.HalfPi + AngularMeasure.Pi);
+        Assert.True(AngularMeasure.HalfPi + AngularMeasure.Pi <= AngularMeasure.Tau);
+
+        Assert.True(AngularMeasure.HalfPi > AngularMeasure.Zero);
+        Assert.True(AngularMeasure.Pi > AngularMeasure.HalfPi);
+        Assert.True(AngularMeasure.HalfPi + AngularMeasure.Pi > AngularMeasure.Pi);
+        Assert.True(AngularMeasure.Tau > AngularMeasure.HalfPi + AngularMeasure.Pi);
+
+        Assert.True(AngularMeasure.HalfPi >= AngularMeasure.Zero);
+        Assert.True(AngularMeasure.Pi >= AngularMeasure.HalfPi);
+        Assert.True(AngularMeasure.HalfPi + AngularMeasure.Pi >= AngularMeasure.Pi);
+        Assert.True(AngularMeasure.Tau >= AngularMeasure.HalfPi + AngularMeasure.Pi);
+    }
+
+    [Theory(DisplayName = "测试角的转换。")]
+    [MemberData(nameof(AngleData))]
+    public void ConvertTest(AngularMeasure angle)
+    {
+        Assert.Equal(angle, AngularMeasure.FromDegree(angle.Degree), NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(angle, AngularMeasure.FromRadian(angle.Radian), NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Theory(DisplayName = "测试三角函数。")]
+    [MemberData(nameof(AngleData))]
+    public void TrigonometricFunctions(AngularMeasure angle)
+    {
+        Assert.Equal(Math.Sin(angle.Radian), angle.Sin(), NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(Math.Cos(angle.Radian), angle.Cos(), NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(Math.Tan(angle.Radian), angle.Tan(), NumericsEqualHelper.IsAlmostEqual);
+
+        Assert.Equal(1, angle.Sin().Square() + angle.Cos().Square(), NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(angle.Sin() / angle.Cos(), angle.Tan(), NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Theory(DisplayName = "测试角的单位向量。")]
+    [MemberData(nameof(AngleData))]
+    public void UnitVectorTest(AngularMeasure angle)
+    {
+        var unitVector = angle.UnitVector;
+
+        Assert.Equal(angle, unitVector.Angle.Normalized, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(1.0, unitVector.Length, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Theory(DisplayName = "测试角的规范化。")]
+    [InlineData(0, 0)]
+    [InlineData(Math.Tau, 0)]
+    [InlineData(Math.Tau / 2, Math.Tau / 2)]
+    [InlineData(- Math.Tau / 4, Math.Tau * 3 / 4)]
+    [InlineData(Math.Tau * 3 / 2, Math.Tau / 2)]
+    public void NormalizedTest(double radian, double expected)
+    {
+        var angle = AngularMeasure.FromRadian(radian);
+        Assert.Equal(expected, angle.Normalized.Radian, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    #endregion
+
+    public static readonly TheoryData<AngularMeasure> AngleData = new([
+        AngularMeasure.Zero,
+        AngularMeasure.HalfPi,
+        AngularMeasure.Pi,
+        AngularMeasure.HalfPi + AngularMeasure.Pi,
+        AngularMeasure.Tau,
+        AngularMeasure.FromRadian(1),
+        AngularMeasure.FromDegree(45),
+        AngularMeasure.FromDegree(1),
+        AngularMeasure.FromDegree(100),
+    ]);
+
+    public static readonly TheoryData<AngularMeasure, AngularMeasure, AngularMeasure> AngleAddTestData = new()
+    {
+        { AngularMeasure.Degree90, AngularMeasure.Degree90, AngularMeasure.Degree180 },
+        { AngularMeasure.Degree90, AngularMeasure.Degree180, AngularMeasure.Degree270 },
+        { AngularMeasure.Degree180, AngularMeasure.Degree180, AngularMeasure.Degree360 },
+        { AngularMeasure.Degree270, AngularMeasure.Degree90, AngularMeasure.Degree360 },
+        { AngularMeasure.HalfPi, AngularMeasure.Pi, AngularMeasure.OneAndHalfPi },
+    };
+}
--- SeWzc.Numerics/AngularMeasure.cs
+++ SeWzc.Numerics/AngularMeasure.cs
@@ -5,6 +5,55 @@
 /// </summary>
 public readonly record struct AngularMeasure
 {
+    #region 静态变量
+
+    /// <summary>
+    /// 零角。
+    /// </summary>
+    public static readonly AngularMeasure Zero = new(0);
+
+    /// <summary>
+    /// π/2 角。
+    /// </summary>
+    public static readonly AngularMeasure HalfPi = new(Math.PI / 2);
+
+    /// <summary>
+    /// π 角。
+    /// </summary>
+    public static readonly AngularMeasure Pi = new(Math.PI);
+
+    /// <summary>
+    /// 3π/2 角。
+    /// </summary>
+    public static readonly AngularMeasure OneAndHalfPi = new(Math.PI * 3 / 2);
+
+    /// <summary>
+    /// 2π 角。
+    /// </summary>
+    public static readonly AngularMeasure Tau = new(Math.Tau);
+
+    /// <summary>
+    /// 90° 角。
+    /// </summary>
+    public static readonly AngularMeasure Degree90 = FromDegree(90);
+
+    /// <summary>
+    /// 180° 角。
+    /// </summary>
+    public static readonly AngularMeasure Degree180 = FromDegree(180);
+
+    /// <summary>
+    /// 270° 角。
+    /// </summary>
+    public static readonly AngularMeasure Degree270 = FromDegree(270);
+
+    /// <summary>
+    /// 360° 角。
+    /// </summary>
+    public static readonly AngularMeasure Degree360 = FromDegree(360);
+
+    #endregion
+
     #region 静态方法
 
     /// <summary>
@@ -47,7 +96,7 @@ public readonly record struct AngularMeasure
     public Vector2D UnitVector => new(Math.Cos(Radian), Math.Sin(Radian));
 
     /// <summary>
-    /// 将角转换为 0 到 2π 之间的角。
+    /// 将角转换为 0 到 2π 之间的角。不包括 2π。
     /// </summary>
     public AngularMeasure Normalized => FromRadian(Radian >= 0 ? Radian % Math.Tau : Radian % Math.Tau + Math.Tau);
 
@@ -58,6 +107,43 @@ public readonly record struct AngularMeasure
     private AngularMeasure(double radian)
     {
         Radian = radian;
+    }
+
+    #endregion
+
+    #region 成员方法
+
+    /// <summary>
+    /// 返回角的余弦值。
+    /// </summary>
+    /// <returns></returns>
+    public double Cos()
+    {
+        return Math.Cos(Radian);
+    }
+
+    /// <summary>
+    /// 返回角的正弦值。
+    /// </summary>
+    /// <returns></returns>
+    public double Sin()
+    {
+        return Math.Sin(Radian);
+    }
+
+    /// <summary>
+    /// 返回角的正切值。
+    /// </summary>
+    /// <returns></returns>
+    public double Tan()
+    {
+        return Math.Tan(Radian);
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"{Radian} rad";
     }
 
     #endregion
@@ -92,6 +178,26 @@ public readonly record struct AngularMeasure
     public static double operator /(AngularMeasure measure1, AngularMeasure measure2)
     {
         return measure1.Radian / measure2.Radian;
+    }
+
+    public static bool operator <(AngularMeasure measure1, AngularMeasure measure2)
+    {
+        return measure1.Radian < measure2.Radian;
+    }
+
+    public static bool operator >(AngularMeasure measure1, AngularMeasure measure2)
+    {
+        return measure1.Radian > measure2.Radian;
+    }
+
+    public static bool operator <=(AngularMeasure measure1, AngularMeasure measure2)
+    {
+        return measure1.Radian <= measure2.Radian;
+    }
+
+    public static bool operator >=(AngularMeasure measure1, AngularMeasure measure2)
+    {
+        return measure1.Radian >= measure2.Radian;
     }
 
     #endregion
