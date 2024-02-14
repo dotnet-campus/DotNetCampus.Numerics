42f0cbb9a09bd01dd56d298bb96cee215f7409ae
SeWZC <36623158+SeWZC@users.noreply.github.com>
2024-02-15 00:53:07 +0800

    删除 Point 中多余的方法，添加注释看，修正命名空间

--- SeWzc.Numerics.Geometry/IPoint.cs
+++ SeWzc.Numerics.Geometry/IPoint.cs
@@ -1,6 +1,6 @@
 ﻿using System.Numerics;
 
-namespace SeWzc.Numerics;
+namespace SeWzc.Numerics.Geometry;
 
 public interface IPoint<TSelf, TVector, TNum>
     where TSelf : unmanaged, IPoint<TSelf, TVector, TNum>
@@ -9,24 +9,74 @@ public interface IPoint<TSelf, TVector, TNum>
 {
     #region 运算符重载
 
-    public static abstract TSelf operator +(TSelf point1, TSelf point2);
-
+    /// <summary>
+    /// 一个点加上一个向量得到新的点。
+    /// </summary>
+    /// <param name="point">要加上向量的点。</param>
+    /// <param name="vector">要加上的向量。</param>
+    /// <returns>相加后得到的新点。</returns>
     public static abstract TSelf operator +(TSelf point, TVector vector);
 
+    /// <summary>
+    /// 一个点加上一个向量得到新的点。
+    /// </summary>
+    /// <param name="vector">要加上的向量。</param>
+    /// <param name="point">要加上向量的点。</param>
+    /// <returns>相加后得到的新点。</returns>
     public static abstract TSelf operator +(TVector vector, TSelf point);
 
+    /// <summary>
+    /// 两个点之间的差向量。
+    /// </summary>
+    /// <param name="point1">向量终点。</param>
+    /// <param name="point2">向量起点。</param>
+    /// <returns>两个点之间的差向量。</returns>
     public static abstract TVector operator -(TSelf point1, TSelf point2);
 
+    /// <summary>
+    /// 一个点减去一个向量得到新的点。
+    /// </summary>
+    /// <param name="point">要减去向量的点。</param>
+    /// <param name="vector">要减去的向量。</param>
+    /// <returns>相减后得到的新点。</returns>
     public static abstract TSelf operator -(TSelf point, TVector vector);
 
-    public static abstract TSelf operator *(TSelf point, double scalar);
+    /// <summary>
+    /// 一个点乘以标量得到新的点。
+    /// </summary>
+    /// <param name="point">要乘以标量的点。</param>
+    /// <param name="scalar">要乘以的标量。</param>
+    /// <returns>相乘后得到的新点。</returns>
+    public static abstract TSelf operator *(TSelf point, TNum scalar);
 
-    public static abstract TSelf operator *(double scalar, TSelf point);
+    /// <summary>
+    /// 标量乘以一个点得到新的点。
+    /// </summary>
+    /// <param name="scalar">要乘以的标量。</param>
+    /// <param name="point">要乘以标量的点。</param>
+    /// <returns>相乘后得到的新点。</returns>
+    public static abstract TSelf operator *(TNum scalar, TSelf point);
 
+    /// <summary>
+    /// 一个点除以标量得到新的点。
+    /// </summary>
+    /// <param name="point">要除以标量的点。</param>
+    /// <param name="scalar">要除以的标量。</param>
+    /// <returns>相除后得到的新点。</returns>
     public static abstract TSelf operator /(TSelf point, double scalar);
 
+    /// <summary>
+    /// 将点转换为向量。
+    /// </summary>
+    /// <param name="point">要转换的点。</param>
+    /// <returns>转换后得到的向量。</returns>
     public static abstract explicit operator TVector(TSelf point);
 
+    /// <summary>
+    /// 将向量转换为点。
+    /// </summary>
+    /// <param name="vector">要转换的向量。</param>
+    /// <returns>转换后得到的点。</returns>
     public static abstract explicit operator TSelf(TVector vector);
 
     #endregion
--- SeWzc.Numerics.Geometry/Point2D.cs
+++ SeWzc.Numerics.Geometry/Point2D.cs
@@ -1,70 +1,67 @@
-﻿namespace SeWzc.Numerics;
+﻿namespace SeWzc.Numerics.Geometry;
 
+/// <summary>
+/// 2 维点。
+/// </summary>
+/// <param name="X">点的 X 坐标。</param>
+/// <param name="Y">点的 Y 坐标。</param>
 public readonly record struct Point2D(double X, double Y) : IPoint<Point2D, Vector2D, double>
 {
     #region 运算符重载
 
-    public static Point2D operator +(Point2D point1, Point2D point2)
-    {
-        return new Point2D(point1.X + point2.X, point1.Y + point2.Y);
-    }
-
+    /// <inheritdoc />
     public static Point2D operator +(Point2D point, Vector2D vector)
     {
         return new Point2D(point.X + vector.X, point.Y + vector.Y);
     }
-
+    
+    /// <inheritdoc />
     public static Point2D operator +(Vector2D vector, Point2D point)
     {
         return new Point2D(point.X + vector.X, point.Y + vector.Y);
     }
-
+    
+    /// <inheritdoc />
     public static Vector2D operator -(Point2D point1, Point2D point2)
     {
         return new Vector2D(point1.X - point2.X, point1.Y - point2.Y);
     }
-
+    
+    /// <inheritdoc />
     public static Point2D operator -(Point2D point, Vector2D vector)
     {
         return new Point2D(point.X - vector.X, point.Y - vector.Y);
     }
-
+    
+    /// <inheritdoc />
     public static Point2D operator *(Point2D point, double scalar)
     {
         return new Point2D(point.X * scalar, point.Y * scalar);
     }
-
+    
+    /// <inheritdoc />
     public static Point2D operator *(double scalar, Point2D point)
     {
         return new Point2D(point.X * scalar, point.Y * scalar);
     }
-
+    
+    /// <inheritdoc />
     public static Point2D operator /(Point2D point, double scalar)
     {
         return new Point2D(point.X / scalar, point.Y / scalar);
     }
-
+    
+    /// <inheritdoc />
     public static explicit operator Vector2D(Point2D point)
     {
         return new Vector2D(point.X, point.Y);
     }
-
+    
+    /// <inheritdoc />
     public static explicit operator Point2D(Vector2D vector)
     {
         return new Point2D(vector.X, vector.Y);
     }
-
-#if WPF
-    public static implicit operator Point(Point2D point)
-    {
-        return new Point(point.X, point.Y);
-    }
-
-    public static implicit operator Point2D(Point point)
-    {
-        return new Point2D(point.X, point.Y);
-    }
-#endif
 
     #endregion
 }
