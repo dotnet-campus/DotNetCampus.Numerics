b5b639f68f3ef0c974b0fe3c63e63b38ee231daf
SeWZC <36623158+SeWZC@users.noreply.github.com>
2024-02-19 22:02:11 +0800

    为 SeWzc.Numerics.sln.DotSettings 添加文件结构布局设置；格式化解决方案

--- SeWzc.Numerics.Geometry/Point2D.cs
+++ SeWzc.Numerics.Geometry/Point2D.cs
@@ -14,49 +14,49 @@ public readonly record struct Point2D(double X, double Y) : IPoint<Point2D, Vect
     {
         return new Point2D(point.X + vector.X, point.Y + vector.Y);
     }
-    
+
     /// <inheritdoc />
     public static Point2D operator +(Vector2D vector, Point2D point)
     {
         return new Point2D(point.X + vector.X, point.Y + vector.Y);
     }
-    
+
     /// <inheritdoc />
     public static Vector2D operator -(Point2D point1, Point2D point2)
     {
         return new Vector2D(point1.X - point2.X, point1.Y - point2.Y);
     }
-    
+
     /// <inheritdoc />
     public static Point2D operator -(Point2D point, Vector2D vector)
     {
         return new Point2D(point.X - vector.X, point.Y - vector.Y);
     }
-    
+
     /// <inheritdoc />
     public static Point2D operator *(Point2D point, double scalar)
     {
         return new Point2D(point.X * scalar, point.Y * scalar);
     }
-    
+
     /// <inheritdoc />
     public static Point2D operator *(double scalar, Point2D point)
     {
         return new Point2D(point.X * scalar, point.Y * scalar);
     }
-    
+
     /// <inheritdoc />
     public static Point2D operator /(Point2D point, double scalar)
     {
         return new Point2D(point.X / scalar, point.Y / scalar);
     }
-    
+
     /// <inheritdoc />
     public static explicit operator Vector2D(Point2D point)
     {
         return new Vector2D(point.X, point.Y);
     }
-    
+
     /// <inheritdoc />
     public static explicit operator Point2D(Vector2D vector)
     {
