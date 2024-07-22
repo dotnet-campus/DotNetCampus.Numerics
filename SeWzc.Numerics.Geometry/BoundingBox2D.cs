c615e3b7e6b63142db9440e97130760b0cda8ced
SeWZC <36623158+SeWZC@users.noreply.github.com>
2024-07-22 17:51:51 +0800

    更新 BoundingBox2D，添加一些方法

--- SeWzc.Numerics.Geometry/BoundingBox2D.cs
+++ SeWzc.Numerics.Geometry/BoundingBox2D.cs
@@ -5,6 +5,15 @@
 /// </summary>
 public readonly record struct BoundingBox2D
 {
+    #region 静态变量
+
+    /// <summary>
+    /// 空的 2 维边界框。
+    /// </summary>
+    public static BoundingBox2D Empty { get; } = new();
+
+    #endregion
+
     #region 静态方法
 
     /// <summary>
@@ -18,7 +27,47 @@ public readonly record struct BoundingBox2D
     public static BoundingBox2D Create(double minX, double minY, double maxX, double maxY)
     {
         if (minX > maxX || minY > maxY)
+        {
             throw new ArgumentException("The minimum value of the bounding box must be less than or equal to the maximum value.");
+        }
+
+        return new BoundingBox2D(minX, minY, maxX, maxY);
+    }
+
+    /// <summary>
+    /// 通过一个点创建 2 维边界框。
+    /// </summary>
+    /// <param name="point">点。</param>
+    /// <returns>宽高为 0 但不为空的 2 维边界框。</returns>
+    public static BoundingBox2D Create(Point2D point)
+    {
+        return new BoundingBox2D(point.X, point.Y, point.X, point.Y);
+    }
+
+    /// <summary>
+    /// 通过点列表创建 2 维边界框。
+    /// </summary>
+    /// <param name="points">点列表。</param>
+    /// <returns>包含所有点的最小 2 维边界框。如果没有坐标，则返回一个空的 2 维边界框。</returns>
+    public static BoundingBox2D Create(IReadOnlyCollection<Point2D> points)
+    {
+        ArgumentNullException.ThrowIfNull(points);
+        if (points.Count == 0)
+        {
+            return Empty;
+        }
+
+        var minX = double.MaxValue;
+        var minY = double.MaxValue;
+        var maxX = double.MinValue;
+        var maxY = double.MinValue;
+        foreach (var point in points)
+        {
+            minX = Math.Min(minX, point.X);
+            maxX = Math.Max(maxX, point.X);
+            minY = Math.Min(minY, point.Y);
+            maxY = Math.Max(maxY, point.Y);
+        }
 
         return new BoundingBox2D(minX, minY, maxX, maxY);
     }
@@ -57,9 +106,19 @@ public readonly record struct BoundingBox2D
     public double MaxY { get; }
 
     /// <summary>
-    /// 边界框是否为空。
+    /// 边界框是否为空。宽高为 0 的边界框不一定为空。
     /// </summary>
     public bool IsEmpty => !_isNotEmpty;
+
+    /// <summary>
+    /// 宽度。
+    /// </summary>
+    public double Width => MaxX - MinX;
+
+    /// <summary>
+    /// 高度。
+    /// </summary>
+    public double Height => MaxY - MinY;
 
     #endregion
 
@@ -100,9 +159,14 @@ public readonly record struct BoundingBox2D
     public BoundingBox2D Union(BoundingBox2D other)
     {
         if (IsEmpty)
+        {
             return other;
+        }
+
         if (other.IsEmpty)
+        {
             return this;
+        }
 
         return Create(
             Math.Min(MinX, other.MinX),
