2cbf77bbcaf84a42d530caac0d21df642679e89e
SeWZC <36623158+SeWZC@users.noreply.github.com>
2024-08-05 00:39:05 +0800

    实现椭圆的仿射变换

---
+++ SeWzc.Numerics.Geometry.Tests/Ellipse2DTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace SeWzc.Numerics.Geometry.Tests;
+
+[TestSubject(typeof(Ellipse2D))]
+public class Ellipse2DTest
+{
+    [Fact(DisplayName = "测试椭圆的创建。")]
+    public void CreateTest()
+    {
+        var ellipse = new Ellipse2D(new Point2D(3, 4), 5, 6, AngularMeasure.FromDegree(30));
+        Assert.Equal(3, ellipse.Center.X);
+        Assert.Equal(4, ellipse.Center.Y);
+        Assert.Equal(6, ellipse.A);
+        Assert.Equal(5, ellipse.B);
+        Assert.Equal(30, ellipse.Angle.Degree, (a, b) => a.IsAlmostEqual(b));
+    }
+
+    [Fact(DisplayName = "测试椭圆的轴端点。")]
+    public void AxisEndpointTest()
+    {
+        var ellipse = new Ellipse2D(new Point2D(3, 4), 5, 6, AngularMeasure.FromDegree(30));
+        Assert.Equal(3 + 6 * Math.Cos(Math.PI / 6), ellipse.AxisEndpointA.X, 10);
+        Assert.Equal(4 + 6 * Math.Sin(Math.PI / 6), ellipse.AxisEndpointA.Y, 10);
+        Assert.Equal(3 + 5 * Math.Cos(Math.PI * 4 / 6), ellipse.AxisEndpointB.X, 10);
+        Assert.Equal(4 + 5 * Math.Sin(Math.PI * 4 / 6), ellipse.AxisEndpointB.Y, 10);
+    }
+
+    [Fact(DisplayName = "测试椭圆的变换。")]
+    public void TransformTest()
+    {
+        var ellipse = new Ellipse2D(new Point2D(), 5, 3, AngularMeasure.Zero);
+        var transformation = AffineTransformation2D.Identity
+            .Scale(new Scaling2D(2, 3))
+            .Rotate(AngularMeasure.FromDegree(30));
+        var transformedEllipse = ellipse.Transform(transformation);
+        Assert.Equal(0, transformedEllipse.Center.X);
+        Assert.Equal(0, transformedEllipse.Center.Y);
+        Assert.Equal(10, transformedEllipse.A, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(9, transformedEllipse.B, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(AngularMeasure.FromDegree(30), transformedEllipse.Angle, (a, b) => a.IsAlmostEqual(b));
+    }
+}
--- SeWzc.Numerics.Geometry/Ellipse2D.cs
+++ SeWzc.Numerics.Geometry/Ellipse2D.cs
@@ -1,25 +1,20 @@
-﻿namespace SeWzc.Numerics.Geometry;
+﻿using SeWzc.Numerics.Equations;
+using SeWzc.Numerics.Matrix;
+
+namespace SeWzc.Numerics.Geometry;
 
 /// <summary>
 /// 椭圆。
 /// </summary>
-/// <param name="Center">椭圆的中心。</param>
-/// <param name="A">椭圆的 A 方向半轴。</param>
-/// <param name="B">椭圆的 B 方向半轴。</param>
-/// <param name="Angle">椭圆的旋转角。</param>
-public readonly record struct Ellipse2D(Point2D Center, double A, double B, AngularMeasure Angle) : IAffineTransformable2D<Ellipse2D>
+public readonly record struct Ellipse2D : IAffineTransformable2D<Ellipse2D>
 {
-    #region 构造函数
+    #region 私有字段
 
-    /// <summary>
-    /// 通过圆创建椭圆。
-    /// </summary>
-    /// <param name="circle"></param>
-    public Ellipse2D(Circle2D circle) : this(circle.Center, circle.Radius, circle.Radius, AngularMeasure.Zero)
-    {
-    }
+    private readonly AngularMeasure _angle;
 
     #endregion
+
+    #region 属性
 
     /// <summary>
     /// 椭圆上轴端点 A。
@@ -31,9 +26,105 @@ public readonly record struct Ellipse2D(Point2D Center, double A, double B, Angu
     /// </summary>
     public Point2D AxisEndpointB => Center + B * (Angle + AngularMeasure.HalfPi).UnitVector;
 
+    /// <summary>
+    /// 椭圆的中心。
+    /// </summary>
+    public Point2D Center { get; }
+
+    /// <summary>
+    /// 椭圆的半长轴。
+    /// </summary>
+    public double A { get; }
+
+    /// <summary>
+    /// 椭圆的半短轴。
+    /// </summary>
+    public double B { get; }
+
+    /// <summary>
+    /// 椭圆的旋转角。范围为 (-π / 2, π / 2]。
+    /// </summary>
+    public AngularMeasure Angle
+    {
+        get => _angle;
+        init => _angle = value.Normalized switch
+        {
+            { Radian: > Math.PI * 3 / 2 } angle => angle - AngularMeasure.Tau,
+            { Radian: > Math.PI / 2 } angle => angle - AngularMeasure.Pi,
+            var angle => angle,
+        };
+    }
+
+    #endregion
+
+    #region 构造函数
+
+    /// <summary>
+    /// 通过圆创建椭圆。
+    /// </summary>
+    /// <param name="circle"></param>
+    public Ellipse2D(Circle2D circle) : this(circle.Center, circle.Radius, circle.Radius, AngularMeasure.Zero)
+    {
+    }
+
+    /// <summary>
+    /// 椭圆。
+    /// </summary>
+    /// <remarks>
+    /// 如果 <paramref name="A" /> 小于 <paramref name="B" />，则会自动交换两者的值。
+    /// </remarks>
+    /// <param name="Center">椭圆的中心。</param>
+    /// <param name="A">椭圆的半长轴。</param>
+    /// <param name="B">椭圆的半短轴。</param>
+    /// <param name="Angle">椭圆的旋转角。</param>
+    public Ellipse2D(Point2D Center, double A, double B, AngularMeasure Angle)
+    {
+        this.Center = Center;
+        this.A = Math.Max(A, B);
+        this.B = Math.Min(A, B);
+        this.Angle = Angle;
+    }
+
+    #endregion
+
+    #region 成员方法
+
     /// <inheritdoc />
     public Ellipse2D Transform(AffineTransformation2D transformation)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(transformation);
+
+        // 计算单位圆变换到该椭圆的仿射变换矩阵
+        var oldTransform = AffineTransformation2D.Create(Center.ToVector(), Angle, 0, new Scaling2D(A, B));
+        // 计算单位圆变换到新生成的椭圆的仿射变换矩阵
+        var newTransform = oldTransform.Apply(transformation);
+
+        // 新椭圆的中心点
+        var center = (Point2D)newTransform.Translation;
+
+        // 后面计算新生成的椭圆的半长轴、半短轴和旋转角
+
+        // 从仿射变换矩阵中获取线性变换矩阵，不考虑中心点平移
+        var matrix = new Matrix2X2D(newTransform.M11, newTransform.M12, newTransform.M21, newTransform.M22);
+
+        // 获取椭圆的方程 X^T * M * X = 1 中的 M 矩阵
+        var inverse = matrix.Inverse();
+        var inverseTranspose = inverse.Transpose;
+        var m = inverseTranspose * inverse;
+
+        // 计算特征值，以及特征值对应的特征向量
+        var eigenEquation = new QuadraticEquation(1, -(m.M11 + m.M22), m.Determinant);
+        var eigenValue1 = eigenEquation.Root1;
+        var eigenValue2 = eigenEquation.Root2;
+        var eigenVector2 = new Vector2D(m.M12, eigenValue2 - m.M11);
+
+        // 特征值分别是长轴和短轴的平方的倒数，所以获取特征值平方根的倒数即可得到长轴和短轴
+        var b = Math.ReciprocalSqrtEstimate(eigenValue1);
+        var a = Math.ReciprocalSqrtEstimate(eigenValue2);
+        // 旋转是半长轴和 x 轴的夹角，所以获取特征向量的角度即可得到旋转角
+        var rotate = eigenVector2.Angle;
+        return new Ellipse2D(center, a, b, rotate);
     }
+
+    #endregion
 }
