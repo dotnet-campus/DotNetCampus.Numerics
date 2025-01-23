5b0e2b084e652ba3be2ce2f2e912b2391b067134
SeWZC <36623158+SeWZC@users.noreply.github.com>
2025-01-23 10:34:38 +0800

    添加和修复椭圆单元测试

--- DotNetCampus.Numerics.Geometry.Tests/Ellipse2DTest.cs
+++ DotNetCampus.Numerics.Geometry.Tests/Ellipse2DTest.cs
@@ -15,13 +15,13 @@ public class Ellipse2DTest
         Assert.Equal(4, ellipse.Center.Y, NumericsEqualHelper.IsAlmostEqual);
         Assert.Equal(6, ellipse.A, NumericsEqualHelper.IsAlmostEqual);
         Assert.Equal(5, ellipse.B, NumericsEqualHelper.IsAlmostEqual);
-        Assert.Equal(30, ellipse.Angle.Degree, NumericsEqualHelper.IsAlmostEqual);
+        Assert.Equal(-60, ellipse.Angle.Degree, NumericsEqualHelper.IsAlmostEqual);
     }
 
     [Fact(DisplayName = "测试椭圆的轴端点。")]
     public void AxisEndpointTest()
     {
-        var ellipse = new Ellipse2D(new Point2D(3, 4), 5, 6, AngularMeasure.FromDegree(30));
+        var ellipse = new Ellipse2D(new Point2D(3, 4), 6, 5, AngularMeasure.FromDegree(30));
         Assert.Equal(3 + 6 * Math.Cos(Math.PI / 6), ellipse.AxisEndpointA.X, NumericsEqualHelper.IsAlmostEqual);
         Assert.Equal(4 + 6 * Math.Sin(Math.PI / 6), ellipse.AxisEndpointA.Y, NumericsEqualHelper.IsAlmostEqual);
         Assert.Equal(3 + 5 * Math.Cos(Math.PI * 4 / 6), ellipse.AxisEndpointB.X, NumericsEqualHelper.IsAlmostEqual);
@@ -41,5 +41,17 @@ public class Ellipse2DTest
         Assert.Equal(10, transformedEllipse.A, NumericsEqualHelper.IsAlmostEqual);
         Assert.Equal(9, transformedEllipse.B, NumericsEqualHelper.IsAlmostEqual);
         Assert.Equal(AngularMeasure.FromDegree(30), transformedEllipse.Angle, NumericsEqualHelper.IsAlmostEqual);
+    }
+
+    [Fact(DisplayName = "测试椭圆旋转 90 度后的轴端点。")]
+    public void AxisEndpointRotate90Test()
+    {
+        var ellipse = new Ellipse2D(new Point2D(), 4, 6, AngularMeasure.Zero)
+            .Transform(AffineTransformation2D.CreateRotation(AngularMeasure.HalfPi));
+
+        Assert.Equal(6, ellipse.AxisEndpointA.X, NumericsEqualHelper.IsAlmostEqual);
+        Assert.True(ellipse.AxisEndpointA.Y.IsAlmostZero());
+        Assert.True(ellipse.AxisEndpointB.X.IsAlmostZero());
+        Assert.Equal(4, ellipse.AxisEndpointB.Y, NumericsEqualHelper.IsAlmostEqual);
     }
 }
